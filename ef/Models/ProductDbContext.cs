404bd8db5cd8324c11707b7425ab624cd2ebaffe
PinkPigPinkPig <vuquytuan2706@gmail.com>
2023-06-07 15:14:50 +0700

    done

--- ef/Models/ProductDbContext.cs
+++ ef/Models/ProductDbContext.cs
@@ -1,9 +1,14 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace ef.Models
 {
     public class ProductDbContext : DbContext
     {
+        public static readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder => {
+            builder.AddFilter(DbLoggerCategory.Query.Name, LogLevel.Information);
+            builder.AddConsole();
+        });
         public DbSet<Product> products { get; set; }
         private const string connectionString = @"
             Data Source=localhost,1433;
@@ -15,6 +20,7 @@ namespace ef.Models
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseLoggerFactory(loggerFactory);
             optionsBuilder.UseSqlServer(connectionString);
         }
     }
--- ef/Program.cs
+++ ef/Program.cs
@@ -1,5 +1,6 @@
 ﻿using ef.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace ef;
 class Program
@@ -44,7 +45,7 @@ class Program
     {
         using var dbContext = new ProductDbContext();
 
-       var products = new object[] {
+        var products = new object[] {
         new Product() { ProductName = "San pham 3", Provider = "Cong ty 3" },
         new Product() { ProductName = "San pham 4", Provider = "Cong ty 4" },
         new Product() { ProductName = "San pham 5", Provider = "Cong ty 5" },
@@ -58,8 +59,64 @@ class Program
     {
         using var dbContext = new ProductDbContext();
         // Linq
-        var products = dbContext.products.ToList();
-        products.ForEach(product => product.PrintInfo());
+        // var products = dbContext.products.ToList();
+        // products.ForEach(product => product.PrintInfo());
+
+        // var qr = from product in dbContext.products
+        //          where product.ProductId >= 3
+        //          select product;
+
+        // var qr = from product in dbContext.products
+        //          where product.ProductId >= 3
+        //          orderby product.ProductId descending
+        //          select product;
+
+        // qr.ToList().ForEach(product => product.PrintInfo());
+
+        Product product = (from p in dbContext.products
+                           where p.ProductId == 4
+                           select p).FirstOrDefault();
+
+        if (product != null)
+        {
+            product.PrintInfo();
+        }
+    }
+
+    static void UpdateProduct(int id, string newName)
+    {
+        using var dbContext = new ProductDbContext();
+
+        Product product = (from p in dbContext.products
+                           where p.ProductId == id
+                           select p).FirstOrDefault();
+
+        if (product != null)
+        {
+            EntityEntry<Product> entry = dbContext.Entry(product);
+            entry.State = EntityState.Detached;
+            product.ProductName = newName;
+            int number_rows = dbContext.SaveChanges(); //Call if you want update DB, return number of rows changed
+            Console.WriteLine($"Da cap nhat {number_rows} du lieu");
+        }
+    }
+
+    static void DeleteProduct(int id)
+    {
+        using var dbContext = new ProductDbContext();
+
+        Product product = (from p in dbContext.products
+                           where p.ProductId == id
+                           select p).FirstOrDefault();
+
+        if (product != null)
+        {
+            // EntityEntry<Product> entry = dbContext.Entry(product);
+            // entry.State = EntityState.Detached;
+            dbContext.Remove(product);
+            int number_rows = dbContext.SaveChanges(); //Call if you want update DB, return number of rows changed
+            Console.WriteLine($"Da xoa {number_rows} du lieu");
+        }
     }
     static void Main(string[] args)
     {
@@ -72,5 +129,6 @@ class Program
         // CreateDataBase();
         // InsertProduct();
         ReadProducts();
+        // UpdateProduct(1, "Laptop");
     }
 }
