7030d2ec4ea249d29873d3d85f080f4ec0d7ac19
PinkPigPinkPig <vuquytuan2706@gmail.com>
2023-06-08 17:54:22 +0700

    done fluent

--- ef-fluent-api/Models/ShopContext.cs
+++ ef-fluent-api/Models/ShopContext.cs
@@ -6,7 +6,8 @@ namespace ef.Models
 {
     public class ShopContext : DbContext
     {
-        public static readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder => {
+        public static readonly ILoggerFactory loggerFactory = LoggerFactory.Create(builder =>
+        {
             builder.AddFilter(DbLoggerCategory.Query.Name, LogLevel.Information);
             builder.AddConsole();
         });
@@ -24,7 +25,49 @@ namespace ef.Models
             base.OnConfiguring(optionsBuilder);
             optionsBuilder.UseLoggerFactory(loggerFactory);
             optionsBuilder.UseSqlServer(connectionString);
-            optionsBuilder.UseLazyLoadingProxies();
+            // optionsBuilder.UseLazyLoadingProxies();
+
+            Console.WriteLine("OnConfiguring");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            Console.WriteLine("OnModelCreating");
+
+            // var entity = modelBuilder.Entity(typeof(Product));
+            //entity => Fluent api => Product
+
+            // var entity = modelBuilder.Entity<Product>();
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                // entity => Fluent api
+
+                // Table mapping - create table
+                entity.ToTable("Products"); //create product table same [Table("Products")]
+                // PK
+                entity.HasKey(p => p.ProductId); // set Pk = [Key]
+
+                // Index, push performance (only use fluent api)
+                entity.HasIndex(p => p.Price).HasDatabaseName("index-sanpham-price"); //Indexing and name the index
+
+                // Relative
+                entity.HasOne(p => p.Category) //Create relation one
+                      .WithMany()
+                      .HasForeignKey("CateId") // Name FK
+                      .OnDelete(DeleteBehavior.Cascade) // Action when delete FK
+                      .HasConstraintName("SomeName")
+                      ; 
+
+                entity.Property(p => p.Name)
+                      .HasColumnName("title")
+                      .HasColumnType("nvarchar")
+                      .HasMaxLength(60)
+                      .IsRequired(false)
+                      .HasDefaultValue("Ten san pha mac dinh");
+
+            });
         }
     }
 }
